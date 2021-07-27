fe20678341a134e5e638a680990d8ff27fa8f9e0
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-27 14:52:45 +0900

    created gif component and added to the flex element

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Gif from "./Gif";
 
 const App = () => {
   return (
     <div>
-      <div className="left-scene">hi</div>
+      <div className="left-scene">
+        <Gif />
+      </div>
       <div className="right-scene">hi there</div>
     </div>
   );
---
+++ src/components/Gif.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Gif = () => {
+  return (
+    <div className="selected-gif">
+      <img
+        className="gif"
+        src="https://media.giphy.com/media/4JEHyYKmDq9VdaZQAa/giphy.gif"
+      ></img>
+    </div>
+  );
+};
+
+export default Gif;
