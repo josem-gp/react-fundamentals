ec6caecea0873880431b3f42a717f9b6df6251ba
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-07-27 18:10:00 +0900

    finished gif & search flex item

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Gif from "./Gif";
+import SearchBar from "./SearchBar";
+import GifList from "./GifList";
 
 const App = () => {
   return (
     <div>
       <div className="left-scene">
+        <SearchBar />
         <Gif />
       </div>
-      <div className="right-scene">hi there</div>
+      <div className="right-scene">
+        <GifList />
+      </div>
     </div>
   );
 };
